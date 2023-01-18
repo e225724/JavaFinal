421ea04a180e07e1a92fa46af2c9b0ff4e246ac0
tokuyamamorimasa <e225724@eve.u-ryukyu.ac.jp>
2023-01-18 23:01:27 +0900

    report5

---
+++ report5/app/src/main/java/jp/ac/uryukyu/ie/e225724/Report5.java
@@ -0,0 +1,10 @@
+package jp.ac.uryukyu.ie.e225724;
+
+import java.lang.NullPointerException;
+
+public class Report5 {
+    public static void main(String[] args){
+        String str = null;
+        str.length();
+    }
+}
