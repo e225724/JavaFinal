08049cc41c49832acdb452f1e0715e8f917d3f25
tokuyamamorimasa <e225724@eve.u-ryukyu.ac.jp>
2023-01-19 02:44:10 +0900

    step3

--- report5/app/src/main/java/jp/ac/uryukyu/ie/e225724/Report5.java
+++ report5/app/src/main/java/jp/ac/uryukyu/ie/e225724/Report5.java
@@ -1,15 +1,9 @@
 package jp.ac.uryukyu.ie.e225724;
 
-import java.lang.NullPointerException;
-
 public class Report5 {
     public static void main(String[] args){
-        String str = null;
-        try{
-            str.length();
-        }catch (NullPointerException e) {
-            System.out.println("エラーが発生しました。");
-            System.out.println(e.getMessage());
-        }
+        String str = "壱百満満";
+        int value = Integer.parseInt(str);
+        System.out.println(value);
     }
 }
