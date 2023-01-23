a02ebc467362535ca81fb67b81f85b1877f7f799
tokuyamamorimasa <e225724@eve.u-ryukyu.ac.jp>
2023-01-23 23:43:15 +0900

    コメント

--- report6/app/src/main/java/jp/ac/uryukyu/ie/e225724/Roulette.java
+++ report6/app/src/main/java/jp/ac/uryukyu/ie/e225724/Roulette.java
@@ -3,22 +3,36 @@ package jp.ac.uryukyu.ie.e225724;
 import java.util.Random;
 import java.io.*;
 
-//１からプレイヤーが選んだ数字までの数がランダムで出力されるクラス
-public class Roulette {
 
+public class Roulette {
+    /**
+     * １からプレイヤーが選んだ数字までの数がランダムで出力されるクラス
+     * @param minValue ランダムの最低値
+     * @param maxValue ランダムの最高値
+     **/
     private int minValue = 1;
     private int maxValue;
 
     public void setMaxValue(int maxValue) {
+        /**
+         * フィールドmaxValueを上書きできるようにしている 
+        **/
         this.maxValue = maxValue;
     }
 
     public int getMaxValue() {
+        /**
+         * maxValueのフィールド設定
+        **/
         return this.maxValue;
     }
 
-    /*ルーレットの値を数値として取り出す */
     public int getIntRouletteValue() {
+        /**
+         * ルーレットの値を数値として取り出す
+         * @param random ランダム関数を使えるようにしている
+         * try-catchで0を出力したときにキャッチできるようにしている
+        **/
         Random random = new Random();//インスタンス作成
         try{
         System.out.println(this.minValue + "~" + getMaxValue() + "のルーレットの結果は");
